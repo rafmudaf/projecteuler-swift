c45daccd367cae625b31c071a0df2ef9e1b618c2
Rafael M Mudafort <rafmudaf@gmail.com>
2017-07-03 11:28:47 -0600

    add general tools directory

---
+++ euler/euler.swift
@@ -0,0 +1,15 @@
+//
+//  euler.swift
+//  projecteuler_swift
+//
+//  Created by Rafael M Mudafort on 7/3/17.
+//  Copyright © 2017 RiffRaff. All rights reserved.
+//
+
+import Foundation
+
+class euler {
+    class func dividesEvenly(_ a: Int, into b: Int) -> Bool {
+        return b % a == 0
+    }
+}
