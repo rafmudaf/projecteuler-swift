fb142e5675733f11eead7ea74f4ab3dc9945a1e2
Rafael M Mudafort <rafmudaf@gmail.com>
2018-05-14 10:27:45 -0600

    add problem 7

---
+++ problem7/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  problem7
+//
+//  Created by Mudafort, Rafael on 4/6/18.
+//  Copyright © 2018 RiffRaff. All rights reserved.
+//
+
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10 001st prime number?
+*/
+
+import Foundation
+
+print("Hello, World!")
