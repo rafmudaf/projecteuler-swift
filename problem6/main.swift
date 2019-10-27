ca3dd2775eec7faf83ac39a3c0bc3128a6f88e59
Rafael M Mudafort <rafmudaf@gmail.com>
2019-10-27 14:20:10 -0600

    Port Problem 6 to the latest MetalInstance

--- problem6/main.swift
+++ problem6/main.swift
@@ -25,45 +25,47 @@ import MetalKit
 var nmax: UInt32 = 100
 var sumsquares: UInt32 = 0
 var sum: UInt32 = 0
-var inputarray: [UInt32] = []
+var inputArray: [UInt32] = []
 for i in 1...nmax {
-    inputarray.append(UInt32(i))
+    inputArray.append(UInt32(i))
 }
-var resultarray = [UInt32](repeating: UInt32(0.0), count: inputarray.count)
+var resultArray = [UInt32](repeating: UInt32(0.0), count: inputArray.count)
 
-let metalInstance = try MetalInstance(kernelName: "squareValueShader")
-
-metalInstance.bindToBuffer(inputArray: inputarray, outputArray: resultarray)
-metalInstance.configurePipeline(threadGroupCountWidth: 10, threadGroupCountHeight: 1, threadGroupCountDepth: 1, threadGroupsWidth: 10, threadGroupsHeight: 1, threadGroupsDepth: 1)
+let metalInstance = try MetalInstance(
+    kernelName: "squareValueShader",
+    inputArray: inputArray,
+    outputArray: resultArray
+)
+metalInstance.configurePipeline(
+    threadGroupCountWidth: 10,
+    threadGroupCountHeight: 1,
+    threadGroupCountDepth: 1,
+    threadGroupsWidth: 10,
+    threadGroupsHeight: 1,
+    threadGroupsDepth: 1
+)
 metalInstance.execute()
 
-// put the result data into a Swift array
-var data = NSData(bytesNoCopy: metalInstance.outputVectorBuffer!.contents(), length: inputarray.count * MemoryLayout.size(ofValue: inputarray[0]), freeWhenDone: false)
-data.getBytes(&resultarray, length: inputarray.count * MemoryLayout.size(ofValue: inputarray[0]))
+if let data = metalInstance.dataAsArray() {
+    data.getBytes(
+        &resultArray,
+        length: inputArray.count * MemoryLayout.size(ofValue: inputArray[0])
+    )
 
-// compute the sum
-sum = inputarray.reduce(0, { x, y in
-    x + y
-})
+    // compute the sum
+    sum = inputArray.reduce(0, { x, y in
+        x + y
+    })
 
-// compute the sum of squares
-sumsquares = resultarray.reduce(0, { x, y in
-    x + y
-})
+    // compute the sum of squares
+    sumsquares = resultArray.reduce(0, { x, y in
+        x + y
+    })
 
-// This is the cpu implementation:
-//var nmax: UInt64 = 100
-//var sumsquares: UInt64 = 0
-//var sum: UInt64 = 0
-//sum = 0
-//sumsquares = 0
-//for i in 0...nmax {
-//    sumsquares += UInt32(Int(pow(Double(i),2)))
-//    sum += i
-//}
-
-// print results
-print("sum of squares - \(sumsquares)")
-print("square of sums - \(pow(Double(sum),2))")
-print("sum square diff - \(pow(Double(sum),2) - Double(sumsquares))")
-
+    // print results
+    print("sum of squares - \(sumsquares)")
+    print("square of sums - \(pow(Double(sum),2))")
+    print("sum square diff - \(pow(Double(sum),2) - Double(sumsquares))")
+} else {
+    print("Error converting data from GPU kernel to NSData")
+}
